125d0e30406217a28b788f4e77f59d69e4b7a33b
Sapdotten <semenova.a.p.04@gmail.com>
2023-12-21 00:38:28 +0400

    Сортировка бинарными вставками

--- sorts.cpp
+++ sorts.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void SimpleInclusionSort(vector<int> &arr)
+// Сортировка простым включением
 {
     for (int i = 1; i < arr.size(); ++i)
     {
@@ -17,14 +18,52 @@ void SimpleInclusionSort(vector<int> &arr)
     }
 }
 
+void InsertElement(vector<int> &arr, vector<int>::iterator start, vector<int>::iterator end, int elem)
+// Вставляет элемент в отсортированный массив с помощью бинарного поиска
+{
+    while (start - end > 0)
+    {
+        size_t i = (end - start) / 2;
+        if (*(start + i) == elem)
+        {
+            arr.insert(start + i, elem);
+            break;
+        }
+        else if (*(start + i) > elem)
+        {
+            end = start + i;
+        }
+        else
+        {
+            start = start + i + 1;
+        }
+    }
+    arr.insert(start, elem);
+}
+void BinaryInsertSort(vector<int> &arr)
+// Бинарная сортировка - вариант сортировки включениями
+// Отличается тем, что в ней вставка элемента в отсортированную часть
+// производится с помощью бинарного поиска
+{
+    for (size_t i = 1; i < arr.size(); ++i)
+    {
+        int elem = arr.at(i);
+        arr.erase(arr.begin() + i);
+        InsertElement(arr, arr.begin(), arr.begin() + i, elem);
+    }
+}
+
 int main()
 {
-    int arr_s[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    vector<int> arr(arr_s, arr_s + sizeof(arr_s) / sizeof(arr_s[0]));
-    SimpleInclusionSort(arr);
-    for (int i = 0; i < arr.size(); ++i)
+    vector<int> arr;
+    for (int i = 10; i >= 0; --i)
     {
-        cout << arr[i] << endl;
+        arr.push_back(i);
+    }
+    BinaryInsertSort(arr);
+    for (size_t i = 0; i < arr.size(); ++i)
+    {
+        cout << arr.at(i) << endl;
     }
     return 0;
 }
